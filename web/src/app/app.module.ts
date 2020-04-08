45ec309095f1368314abb3e8d5fc88297b1653ec
Ron Shah <ron@insight.ly>
2020-04-07 21:59:02 -0700

    + Base Contact Component + Angular FlexLayoutModule + Localize + Angular Materials - bootstrap

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,23 +4,41 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CdkTableModule} from '@angular/cdk/table';
 import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 /* Components */
 import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
 import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {ContactComponent} from './components/contact/contact.component';
 
+/* Angular Materials */
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     CdkTableModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
