d5b84b907206cfd5952f5265a47ca7d77d7d24c3
rons <ronakcshah.23@gmail.com>
2026-08-11 18:40:08 -0700

    Moved test to JUnit5 and minor changes to the path on HTML5Controller.

--- src/main/java/personal/utilities/Html5PathsController.java
+++ src/main/java/personal/utilities/Html5PathsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Html5PathsController {
 
-    @RequestMapping(value = "/{[path:[^\\.]*}")
+    @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/index.html";
     }
--- src/test/java/personal/TestApplicationTests.java
+++ src/test/java/personal/TestApplicationTests.java
@@ -1,21 +1,18 @@
 package personal;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URL;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestApplicationTests {
 
@@ -28,7 +25,7 @@ public class TestApplicationTests {
     @Autowired
     private TestRestTemplate template;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         this.base = new URL("http://localhost:" + port + "/");
     }
